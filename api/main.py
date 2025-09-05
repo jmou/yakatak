2b3574e7f4c27c82e918e17cca0efdee3d0a1844
Joe Mou <dev@mou.fo>
2025-09-05 03:30:48 -0400

    api: Set Cache-Control: no-cache

--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
 from pathlib import Path
+from typing import Awaitable, Callable
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
 from fastapi.responses import FileResponse
 from pydantic import DirectoryPath, FilePath
 from pydantic_settings import BaseSettings
@@ -15,19 +16,28 @@ settings = Settings()
 app = FastAPI()
 
 
+@app.middleware("http")
+async def cache_control_middleware(
+    request: Request, call_next: Callable[[Request], Awaitable[Response]]
+) -> Response:
+    response = await call_next(request)
+    response.headers["Cache-Control"] = "no-cache"
+    return response
+
+
 @app.get("/api/deck")
-def get_deck():
+def get_deck() -> Response:
     return FileResponse(settings.deck_json_file, media_type="application/json")
 
 
 @app.get("/api/scrapes/{scrape_id}/thumb")
-def get_scrape_thumb(scrape_id: str):
+def get_scrape_thumb(scrape_id: str) -> Response:
     image_path = settings.scrape_dir / scrape_id / "derived" / "thumb.png"
     return FileResponse(image_path, media_type="image/png")
 
 
 @app.get("/api/scrapes/{scrape_id}/tiles/{tile_index}")
-def get_scrape_tile(scrape_id: str, tile_index: int):
+def get_scrape_tile(scrape_id: str, tile_index: int) -> Response:
     image_path = (
         settings.scrape_dir / scrape_id / "derived" / "tiles" / f"{tile_index}.png"
     )
